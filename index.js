01cd41091e2b3629c0f4150ba2d77b5306c5e329
chris laframboise <hire.code.seegreatness@gmail.com>
2018-11-26 13:17:08 -0700

    updated bot

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var Discord = require("discord.js");
 var client = new Discord.Client();
-var token= process.env.TOKEN
+var token= process.env.TOKEN;
+var prefix= process.env.PREFIX;
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
 });
 
 client.on('message', msg => {
@@ -10,4 +13,14 @@ client.on('message', msg => {
     msg.reply('Pong!');
   }
 });
+
+client.on('message', msg => {
+    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
+    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
+    const args = msg.content.split(' ').slice(1).join(' ');
+    if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
+    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+});
+
+
 client.login(token);
